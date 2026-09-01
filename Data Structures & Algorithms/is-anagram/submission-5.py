b22bdf8db1182f29d3a708b0fe766ea80a3ee70a
pdburro <pdburro04@gmail.com>
2026-09-01 13:29:43 -0500

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+        
+        solution = {}
+
+        for char in s:
+            solution[char] = solution.get(char, 0) + 1
+        
+        for char in t:
+            if char not in solution or solution[char] == 0:
+                return False
+            solution[char] -= 1
+        return True
